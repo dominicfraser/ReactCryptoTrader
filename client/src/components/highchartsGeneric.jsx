b7224f8ab40dcef659ed773645c28db99dc9b097
Dominic <mrdjfraser@gmail.com>
2017-06-16 22:45:39 +0100

    setData function eliminates duplicate code

--- client/src/components/highchartsGeneric.jsx
+++ client/src/components/highchartsGeneric.jsx
@@ -18,29 +18,8 @@ console.log('render highchartsGeneric')
   componentWillReceiveProps(newprops) {
 console.log('componentWillReceiveProps')
 console.log('newprops.options.series[0].data', newprops.options.series[0].data);
-    this.chart.update({ series:[{
-          name: 'AAPL',
-          type: 'area',
-          data: newprops.options.series[0].data,
-          gapSize: 5,
-          tooltip: {
-              valueDecimals: 2
-          },
-          fillColor: {
-              linearGradient: {
-                  x1: 0,
-                  y1: 0,
-                  x2: 0,
-                  y2: 1
-              },
-              stops: [
-                  [0, Highcharts.getOptions().colors[0]],
-                  [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-              ]
-          },
-          threshold: null
-      }]  
-    })
+
+    this.chart.series[0].setData(newprops.options.series[0].data)
 
     this.chart.xAxis[0].setExtremes(new Date().setMinutes(new Date().getMinutes()-5), new Date().getTime())
   }
